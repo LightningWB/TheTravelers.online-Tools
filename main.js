2d7399c55e4462491e57ed402ecf021648be8581
LightningWB <63766407+LightningWB@users.noreply.github.com>
2020-07-25 19:20:42 -0700

    Added tree mower and auto reconnect
    There is code in for an auto city and house raider that i am working on currently, but I wanted to get the auto reconnect out quick.

--- main.js
+++ main.js
@@ -1,6 +1,59 @@
 // the travelers tools by LightningWB
-// 1.0
+// 1.1
 // I am trying to keep all the code in one file so it is easier to setup
+// this is a small spiral loop function that I modified slightly for my use from Neatsu on stack exchange. https://stackoverflow.com/a/46852039 All credit for this goes to him
+// spiral find
+let spiralFind = (x, y, step, count, target, target2=null, exempt=[]) => {
+    let distance = 0;
+    let range = 1;
+    let direction = 'up';
+
+    for ( let i = 0; i < count; i++ ) {
+        // stuff I added
+        if (!exempt.includes([YOU.x+x,YOU.y+y])){
+            if(document.getElementById((YOU.x+x)+'|'+(YOU.y+y)).textContent==target || document.getElementById((YOU.x+x)+'|'+(YOU.y+y)).textContent==target2){
+                return {found:true, relX:x, relY:y};
+            }
+        }
+        // not my stuff
+        distance++;
+        switch ( direction ) {
+            case 'up':
+                y += step;
+                if ( distance >= range ) {
+                    direction = 'right';
+                    distance = 0;
+                }
+                break;
+            case 'right':
+                x += step;
+                if ( distance >= range ) {
+                    direction = 'bottom';
+                    distance = 0;
+                    range += 1;
+                }
+                break;
+            case 'bottom':
+                y -= step;
+                if ( distance >= range ) {
+                    direction = 'left';
+                    distance = 0;
+                }
+                break;
+            case 'left':
+                x -= step;
+                if ( distance >= range ) {
+                    direction = 'up';
+                    distance = 0;
+                    range += 1;
+                }
+                break;
+            default:
+                break;
+        }
+    }
+    return {found:false, relX:null, relY:null};
+}
 //Xp
 var XPBot={
     startXP(){
@@ -10,10 +63,10 @@ var XPBot={
     },
     afkXP(){
         if(this.turn==="left"){
-            SOCKET.send({action:"setDir", dir:"w", autowalk:false});
+            setDir('w');
             this.turn="right";
         }else{
-            SOCKET.send({action:"setDir", dir:"e", autowalk:false});
+            setDir('e');
             this.turn="left";
         }
     },
@@ -57,16 +110,16 @@ var mineBot={
             SOCKET.send({action: "event_choice", option: "__leave__"});// stop getting stuck on events
             this.direction='w';
             this.pos--;
-            SOCKET.send({action: 'setDir', dir: 'sw', autowalk:false});        }
+            setDir('sw');        }
         else if(this.pos<=0){
             SOCKET.send({action: "event_choice", option: "__leave__"});
             this.direction='e';
             this.pos++;
-            SOCKET.send({action: 'setDir', dir: 'se', autowalk:false});
+            setDir('se');
         }
         else{
             SOCKET.send({action: "event_choice", option: "__leave__"});
-            SOCKET.send({action: "setDir", dir: this.direction, autowalk: false});
+            setDir(this.direction);
             if(this.direction=='e'){this.pos++;}
             else{this.pos--;}
             this.nextMove='dig';
@@ -86,6 +139,8 @@ var travelBot={
         this.timer=setInterval(function(){travelBot.travel();}, 1000);
     },
     travel(){
+        this.xDest=Number(this.xDest);
+        this.yDest=Number(this.yDest);
         SOCKET.send({action: "event_choice", option: "__leave__"});
         if (this.yDest>YOU.y){// north south
             this.travelDir='n';
@@ -172,7 +227,7 @@ var travelBot={
             }
         }
         DSTEP.click();
-        SOCKET.send({action:"setDir", dir:this.travelDir, autowalk:false});
+        setDir(this.travelDir);
     },
     stop(){
         this.running=false;
@@ -214,6 +269,323 @@ var travelBot={
         return dest;
     }
 };
+// tree farmer
+var treeBot={
+    start(){
+        this.running=true;
+        this.timer=setInterval(function(){treeBot.tree()}, 1000);
+    },
+    tree(){
+        SOCKET.send({action: "event_choice", option: "__leave__"});
+        if(this.nextMove==='collect'){
+            HANDS.gettree(document.getElementById('hands-tree'));
+            this.nextMove='back';
+            spiralResults=spiralFind(0, 0, 1, 961, 't');
+            if (spiralResults.found===true){
+                this.nextMove='move';
+                this.targetX=spiralResults.relX;
+                this.targetY=spiralResults.relY;
+                this.prevCoords={x:YOU.x, y:YOU.y};
+            }
+        }
+        else if (this.nextMove==='move'){
+            this.goto(this.targetX, this.targetY)
+        }
+        else if(this.nextMove==='back'){
+            this.goto(this.targetX, this.targetY)
+        }
+        else{
+            spiralResults=spiralFind(0, 0, 1, 961, 't');
+            if (spiralResults.found===true){
+                this.nextMove='move';
+                this.targetX=spiralResults.relX;
+                this.targetY=spiralResults.relY;
+                this.prevCoords={x:YOU.x, y:YOU.y};
+            }
+            else{
+                setDir('e');
+            }
+        }
+    },
+    stop(){
+        clearInterval(this.timer);
+        this.running=false;
+        this.nextMove='travel'
+    },
+    goto(x, y){
+        if (YOU.y+y>YOU.y){// north south
+            travelDir='n';
+            this.targetY=y-1;
+        }
+        else if (YOU.y+y<YOU.y){
+            travelDir='s';
+            this.targetY=y+1;
+        }
+        else{
+            travelDir='';
+        }
+        if (YOU.x+x>YOU.x){// east west
+            travelDir=travelDir+'e';
+            this.targetX=x-1;
+        }
+        else if (YOU.x+x<YOU.x){
+            travelDir=travelDir+'w';
+            this.targetX=x+1;
+        }
+        else{
+            travelDir=travelDir+'';
+        }
+        if(travelDir===''){
+            if (this.nextMove=='move'){
+                this.nextMove='collect';
+            }
+            else if (this.nextMove=='back'){
+                this.nextMove='travel';
+            }
+            return'there';
+        }
+        else{
+            setDir(travelDir);
+            return'notThere';
+        }
+    }
+};
+// Auto city and house
+var autoLoot={
+    getUMF:false,
+    start(){
+        this.running=true;
+        this.timer=setInterval(function(){autoLoot.raid()}, 1000);
+    },
+    raid(){
+        if(this.nextMove=='steal'){
+            if (popup.isOpen==true){// stops if there is a little lag
+                if(POPUP.evTitle=='a desolate city'){// cities
+                    this.placeType='desolate'
+                }
+                else if(POPUP.evTitle=='a walled city'){
+                    this.placeType='walled'
+                }
+                else if(POPUP.evTitle=='a withered city'){
+                    this.placeType='withered'
+                }
+                else if(POPUP.evTitle=='a governing city'){
+                    this.placeType='governing'
+                }
+                else if(POPUP.evTitle=='a blasted city'){
+                    this.placeType='blasted'
+                }
+                else if(POPUP.evTitle=='a barn'){// houses
+                    this.placeType='barn'
+                }
+                else if(POPUP.evTitle=='a cabin'){
+                    this.placeType='cabin'
+                }
+                else if(POPUP.evTitle=='a garage'){
+                    this.placeType='garage'
+                }
+                else if(POPUP.evTitle=='a childcare center'){
+                    this.placeType='childcare'
+                }
+                else if(POPUP.evTitle=='a church'){
+                    this.placeType='church'
+                }
+                else if(POPUP.evTitle=='a humble residence'){
+                    this.placeType='humble'
+                }
+                else if(POPUP.evTitle=='a low home'){
+                    this.placeType='low'
+                }
+                else if(POPUP.evTitle=='an old home'){
+                    this.placeType='old'
+                }
+                else if(POPUP.evTitle=='a pet store'){
+                    this.placeType='pet'
+                }
+                else if(POPUP.evTitle=='a ruined house'){
+                    this.placeType='ruined'
+                }
+                else if(POPUP.evTitle=='a run-down shack'){
+                    this.placeType='run-down'
+                }
+                else if(POPUP.evTitle=='a weatherd house'){
+                    this.placeType='weathered'
+                }
+                this.collect()
+            }
+        }
+        else if(this.nextMove==='collect'){
+            this.collect();
+            this.nextMove='steal';
+            this.targetX=YOU.x-this.prevCoords.x;
+            this.targetY=YOU.y-this.prevCoords.y;
+        }
+        else if (this.nextMove==='move'){
+            this.goto(this.targetX, this.targetY)
+        }
+        else if(this.nextMove==='back'){
+            this.goto(this.targetX, this.targetY)
+        }
+        else{
+            spiralResults=spiralFind(0, 0, 1, 961, 'C', target2='H');
+            if (spiralResults.found===true){
+                this.nextMove='move';
+                this.targetX=spiralResults.relX;
+                this.targetY=spiralResults.relY;
+                this.prevCoords={x:YOU.x, y:YOU.y};
+            }
+            else{
+                setDir('e');
+            }
+        }
+    },
+    stop(){
+        clearInterval(this.timer);
+        this.running=false;
+        this.nextMove='travel'
+    },
+    goto(x, y){
+        if (YOU.y+y>YOU.y){// north south
+            travelDir='n';
+            this.targetY=y-1;
+        }
+        else if (YOU.y+y<YOU.y){
+            travelDir='s';
+            this.targetY=y+1;
+        }
+        else{
+            travelDir='';
+        }
+        if (YOU.x+x>YOU.x){// east west
+            travelDir=travelDir+'e';
+            this.targetX=x-1;
+        }
+        else if (YOU.x+x<YOU.x){
+            travelDir=travelDir+'w';
+            this.targetX=x+1;
+        }
+        else{
+            travelDir=travelDir+'';
+        }
+        if(travelDir===''){
+            if (this.nextMove=='move'){
+                this.nextMove='collect';
+            }
+            else if (this.nextMove=='back'){
+                this.nextMove='travel';
+            }
+            return'there';
+        }
+        else{
+            setDir(travelDir);
+            return'notThere';
+        }
+    },
+    collect(){
+        if(WORLD.deriveTile(YOU.x, YOU.y)=='C'){// city paths
+            switch(this.placeType){
+                case 'desolate':
+                    break;
+                case 'walled':
+                    break;
+                case 'withered':
+                    break;
+                case 'governing':
+                    break;
+                case 'blasted':
+                    break;
+            }
+        }
+        else if(WORLD.deriveTile(YOU.x, YOU.y)=='H'){// house paths
+            switch(this.placeType){
+                case 'barn':
+                    this.leaveEvent();
+                    break;
+                case 'cabin':
+                    if(POPUP.evTitle=='a cabin'){
+                        //add in button here
+                    }
+                    break;
+                case 'childcare':
+                    this.leaveEvent();
+                    break;
+                case 'church':
+                    break;
+                case 'garage':
+                    break;
+                case 'humble':
+                    this.leaveEvent();
+                    break;
+                case 'low':
+                    this.leaveEvent();
+                    break;
+                case 'old':
+                    break;
+                case 'pet':
+                    break;
+                case 'ruined':
+                    break;
+                case 'run-down':
+                    break;
+                case 'weathered':
+                    break;
+            }
+        }
+    },
+    leaveEvent(){
+        classList=document.getElementsByClassName('popup-button')
+        for(i=0;i<classList.length;i++){
+            if(classList[i].value=='exit event'){
+                classList[i].click();
+            }
+        }
+        this.nextMove='back';
+    },
+    changeUMF(){
+        if (document.getElementById('UMF').checked===true){
+            this.getUMF=true;
+        }
+        else{
+            this.getUMF=false
+        }
+    },
+    checkItem(item){
+        if (SUPPLIES.current.hasOwnProperty(item)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
+//auto Reconnect
+var autoReconnect={
+    running:false,
+    toggle(){
+        if (this.running==false){
+            this.running=true;
+            this.timer=setInterval(function(){autoReconnect.run()}, 1000*10);
+        }
+        else{this.stop()}
+        controller.toggelColor('reconnect')
+    },
+    run(){
+        if (SOCKET.isOpen==false){
+            SOCKET.open();
+            setTimeout(()=>{
+            POPUP.hide();
+            SOCKET.send({
+                "action": "setDir",
+                "dir": YOU.dir,
+                "autowalk": YOU.autowalk
+            });}, 1000)
+        }
+    },
+    stop(){
+        this.running=false;
+        clearInterval(this.timer);
+    }
+}
 // controller
 var controller={
     runningList:[],
@@ -236,6 +608,12 @@ var controller={
         else if(job=='travel'){
             travelBot.start();
         }
+        else if(job=='treeBot'){
+            treeBot.start();
+        }
+        else if(job=='autoLoot'){
+            autoLoot.start();
+        }
         this.runningList.push(job);
     },
     // stops
@@ -251,6 +629,12 @@ var controller={
         }
         else if(job=='travel'){
             travelBot.stop();
+        }
+        else if(job=='treeBot'){
+            treeBot.stop();
+        }
+        else if(job=='autoLoot'){
+            autoLoot.stop();
         }
         this.runningList.splice(this.runningList.indexOf(job), 1);// clears list
     },
@@ -288,6 +672,22 @@ var controller={
                 this.start('travel');
             }
         }
+        else if(job=='treeBot'){// tree
+            if (treeBot.running==true){
+                this.stop('treeBot');
+            }
+            else{
+                this.start('treeBot');
+            }
+        }
+        else if(job=='autoLoot'){// city and house
+            if (autoLoot.running==true){
+                this.stop('autoLoot');
+            }
+            else{
+                this.start('autoLoot');
+            }
+        }
         else{
             window.alert('Error: Job not found');
         }
@@ -319,7 +719,7 @@ var color={
             this.textColor='rgb(255, 255, 255)';//white
             this.background='rgb(40, 40, 40)';//gray
             this.clickedTextColor ='rgb(215, 215, 215)';
-            this.clickedBackground='rgb(0, 0, 0)';
+            this.clickedBackground='rgb(83, 83, 83)';
         }
         else{
             this.textColor='rgb(0, 0, 0)';//black
@@ -329,16 +729,20 @@ var color={
         }
     }
 };
-function popup(){
+// help popup
+function popupHelp(){
     POPUP.new('Help', "Afk Xp will walk back and forth. Auto double step will automaticly double step. Auto mine with metal detector will mine if you have a metal detctor equiped and a shovel. Auto travel will automaticly travel to the desired coords. Using a boat is quicker and more reliable. If you don't have a boat equiped you may get stuck.", undefined);
-}
+};
+// new change dir to work with buttons
+function setDir(dir){
+    ENGINE.dir(dir, document.getElementById('arrow-'+dir))
+};
 function init(){
     color.getColor();
     var insertedHTML=document.createElement("div");
     insertedHTML.innerHTML=
     //this is the html added to the bottom of the webpage
-    // the \ is so the string can be multiline and readable
-   `<div id="Utility Hot Bar" style="margin:auto;width:530px;">
+   `<div id="Utility Hot Bar" style="margin:auto;width:636px;">
         <style>
         .tool{
             margin:2px 2px;
@@ -369,9 +773,19 @@ function init(){
             <input type="number" id="x" placeholder="X" onchange="travelBot.changeDestinationX(this.value)" style="width:88px">
             <input type="number" id="y" placeholder="Y" onchange="travelBot.changeDestinationY(this.value)" style="width:88px">
         </div>
-        <div class="tool toolUnClicked" onclick=popup() id="help">Help</div>
+        <div class="tool toolUnClicked" id="treeBot" onclick=controller.toggle('treeBot')>Tree Mower</div>
+        `+/*<div class="tool toolUnClicked" id="autoLoot">
+            <span onclick=controller.toggle('autoLoot') >City/House Raider</span>
+            <br>
+            <label for='UMF'>Get UMF</label>
+            <input type="checkbox" id='UMF' onchange="autoLoot.changeUMF()">
+        </div>*/`
+        <div class="tool toolUnClicked" onclick=autoReconnect.toggle() id="reconnect">Auto Reconnect</div>
+        <div class="tool toolUnClicked" onclick=popupHelp() id="help">Help</div>
     </div>`;
     var target=document.getElementById('game-content').getElementsByClassName('mid-screencontainer scrollbar')[0];
     target.appendChild(insertedHTML);
+    EQUIP.menuEl.style.width="430px";
+    BUILD.boxEl.style.width="430px";
 }
 init();
